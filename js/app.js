731b06e28ba49ee7baa4b24828df4329a0bb4e00
AvishayOmer <a0583255885@gmail.com>
2026-05-27 15:08:58 +0300

    lllk

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import { initMenu } from "./menu.js";
-import { initModal } from "./modal.js";
-import { trackView } from "./analytics.js";
-import { updateUI } from "./ui.js";
-
-initMenu();
-initModal();
-trackView();
-updateUI();
